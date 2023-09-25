886f1da78c5ecf3e4bd088a6c30edf9ea48e3d6a
riko tenten <riko1010@protonmail.ch>
2023-09-25 05:25:14 -0400

    Auto commit

--- assets-php/classes.php
+++ assets-php/classes.php
@@ -464,8 +464,10 @@ set file array default key to null, regex if date string.solves newline, of chat
    if(preg_match($pattern, $buffer, $matches)) {
    $sfd->seek($i + 1);
    if (preg_match($pattern, $sfd->current(), $matches)) {
+   yield ($holdbuffer != null ? $holdbuffer.'\n'.$buffer 
+   :
+     $buffer);  
    $holdbuffer = null;
-   yield $buffer;  
    } else {
    $holdbuffer .= $buffer;
    }
@@ -479,7 +481,9 @@ set file array default key to null, regex if date string.solves newline, of chat
    	/* append assumed chat continuation to previous array, \n or \r\n considered. */
    $sfd->seek($i + 1);
    if (preg_match($pattern, $sfd->current(), $matches)) {
-   yield ($holdbuffer != null ? "$holdbuffer\n$buffer" : $buffer);
+   yield ($holdbuffer != null ? $holdbuffer.'\n'.$buffer 
+   :
+     $buffer);  
    $holdbuffer = null;
    } else {
    $holdbuffer .= $buffer;
