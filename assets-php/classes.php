2881c6676ebba668881aff3e05a2d4accfb43e2c
riko tenten <riko1010@protonmail.ch>
2023-09-25 06:50:16 -0400

    Auto commit

--- assets-php/classes.php
+++ assets-php/classes.php
@@ -414,7 +414,7 @@ print 'iter'.$i.$buffer.'-----<hr/>';
    if (preg_match($pattern, $sfd->current(), $matches)) {
    $holdbuffer .= ($holdbuffer != null ?
    '\n'.$buffer : $buffer);
-   yield $holdbuffer.'not match, next match, yield, next match'.'"'.$sfd->current().'"';  
+   yield json_encode($matches).$holdbuffer.'not match, next match, yield, next match'.'"'.$sfd->current().'"';  
    $holdbuffer = null;
    } else {
    $holdbuffer .= ($holdbuffer != null ?
