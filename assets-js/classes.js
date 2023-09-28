b32aebcca3fc3cbf20a427624f726b248f0a9f92
riko tenten <riko1010@protonmail.ch>
2023-09-28 19:35:42 -0400

    Auto commit

--- assets-js/classes.js
+++ assets-js/classes.js
@@ -36,8 +36,8 @@ PromiseResponse.nrecordsperpage = Ct.recordsperpage;
 if (Ct.TotalDuration > Ct.maxFetchDataDuration) {
 /* adjust total records by response speed for 100 records default min = 25, max = 100 */
 devlog(`${Ct.TotalDuration}  mseconds`);
-Ct.DurationForARecord = Ct.TotalDuration / Ct.recordsperpage;
-Ct.nrecordsperpage = Ct.recordsperpage / Ct.maxFetchDataDuration;
+Ct.RecordsForDurationUnit = Ct.recordsperpage / Ct.TotalDuration;
+Ct.nrecordsperpage = Ct.RecordsForDurationUnit * Ct.maxFetchDataDuration;
 devlog(`New records per page: ${Ct.nrecordsperpage}`);
 if (Ct.nrecordsperpage < Ct.minrecordsperpage) {
 /* set to minimum if connection extremely slow, must overflow vh for trigger reasonably */
