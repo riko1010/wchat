12c9f6e7f50ebb8c218bbaeb2a24a5be9fdbaeca
riko tenten <riko1010@protonmail.ch>
2023-09-28 17:30:05 -0400

    Auto commit

--- index.php
+++ index.php
@@ -82,8 +82,8 @@ $processlines->groupchat = $app->GroupChat;
 $processlines->ChatFile = $app->ChatFile;
 $processlines->dirpath = $app->DirPath;
 $processlines->baseDir = $baseDir;
-$processlines->PaginationFrom = 0;
-$processlines->PaginationTo = $recordsperpage;
+$processlines->PaginationFrom = (int) 0;
+$processlines->PaginationTo = (int) $recordsperpage;
 $processlines->iterable = $app->ChatFileGenerator(
   $processlines->PaginationFrom,
   $processlines->PaginationTo
