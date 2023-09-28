d65b3f8732e232204eac2d01877bb04fe208f4c9
riko tenten <riko1010@protonmail.ch>
2023-09-28 09:10:22 -0400

    Auto commit

--- assets-js/classes.js
+++ assets-js/classes.js
@@ -11,6 +11,7 @@ class infinitescrollrequest {
     return new Promise(function(resolve, reject) {
       /* post request sent was built and network debugger shows the request body built and concatenated with &, type www/urlencoded** data , api shows $_REQUEST to be empty. using get, requests is deprecated, axios examples for post do not work, superagent, fetch, others considered or xmlhttp* */
       let PromiseResponse = {};
+      Classthis.StartTime = Date.now();
       $.ajax({
         method: "GET",
         url: Classthis.url,
@@ -22,6 +23,7 @@ class infinitescrollrequest {
       }).then(function(AjaxResponse) {
         devlog('request done');
         if (AjaxResponse.status == 'success') {
+          Classthis.EndTime = Date.Now();
           $(Classthis.responsecontainer).append(AjaxResponse.response);
           PromiseResponse.npagination = AjaxResponse.pagination;
         } else if (AjaxResponse.status == 'eof') {
--- index.php
+++ index.php
@@ -295,14 +295,13 @@ scene = new ScrollMagic.Scene({triggerElement: "#loader", triggerHook: "onEnter"
 scene.enabled(false);
 $(isr.loadercontainer).removeClass("hidden");
 try {
-  StartTime = Date.now();
   isr.FetchData().then(function(PromiseResponse) {
+    /* success handler */
   isr.pagination = PromiseResponse.npagination;
   isr.FetchDataSuccessHandler(PromiseResponse);
   $(isr.loadercontainer).addClass("hidden");
   }, function(PromiseResponse){
   /* failed request, enable scene event listener */
-  EndTime = Date.now();
   $(isr.loadercontainer).addClass("hidden");
   scene.enabled(true);
   scene.update();  
