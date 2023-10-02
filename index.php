8f3f3c8056c3300a3d7efd23ac9171444a8ae022
riko tenten <riko1010@protonmail.ch>
2023-10-01 20:30:04 -0400

    Auto commit

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 //error_reporting(0);
 require_once 'vendor/autoload.php';
 use Laminas\Config\Config as Config;
-
+/*
 use Whoops\Handler\JsonResponseHandler;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
-
+*/
 $ConfigFile = 'assets-php/settings.php';
 require 'assets-php/classes.php';
 
