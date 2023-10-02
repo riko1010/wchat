9caebb71c77ae93154cb7dea2b1b4bda7ffe44d5
riko tenten <riko1010@protonmail.ch>
2023-10-02 13:50:38 -0400

    Auto commit

--- assets-php/settings.php
+++ assets-php/settings.php
@@ -4,21 +4,23 @@ return [
   'SiteUrl' => 'https://dev.wchat.space',
   'recordsperpage' => 100,
   'unzippyURI' => 'https://wchat.space/python/rununzip.py',
-  'dropboxfolderuriaszip' => 'https://www.dropbox.com/sh/bw5ze45z2essn97/AADWfIO9D7riaye8WwI9mDyNa?dl=1',
-  'dropboxfolderuriaszipheader' => 'https://www.dropbox.com/sh/bw5ze45z2essn97/AADWfIO9D7riaye8WwI9mDyNa?raw=1',
+  'dropboxfolderuriaszip' =>
+    'https://www.dropbox.com/sh/bw5ze45z2essn97/AADWfIO9D7riaye8WwI9mDyNa?dl=1',
+  'dropboxfolderuriaszipheader' =>
+    'https://www.dropbox.com/sh/bw5ze45z2essn97/AADWfIO9D7riaye8WwI9mDyNa?raw=1',
   'PyArchiveURI' => 'https://wchat.space/python/runarchive.py',
   'cfFolder' => 'conversations',
-  'videoPlayerURL' => 'https://onelineplayer.com/player.html?autoplay=true&autopause=true&muted=true&loop=true&url=%s&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true',
+  'videoPlayerURL' =>
+    'https://onelineplayer.com/player.html?autoplay=true&autopause=true&muted=true&loop=true&url=%s&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true',
   'videoPlayerPoster' => 'images/videoposter.png',
   'docViewer' => 'https://docs.google.com/viewer?url=%s&embedded=true',
   'og' => [
     'image' => 'https://dev.wchat.space/images/ogimage.jpg',
-    'contenttype' => 'chat'
-    ],
+    'contenttype' => 'chat',
+  ],
   'cfFilespattern' => '/WhatsApp Chat with\s(?P<name>.*?)((\.\d)?.txt)/i',
   'sitemapxml' => 'sitemap.xml',
   'robotstxt' => 'robots.txt',
   'baseDir' => dirname(__DIR__),
-  'sqlitedb' => dirname(__DIR__).'db.sqlite'
-  ];
- 
+  'sqlitedb' => dirname(__DIR__) . '/' . 'db.sqlite',
+];
