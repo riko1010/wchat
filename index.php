a0819f0fca03ac51aa690ad628e7eef4ef340d02
riko tenten <riko1010@protonmail.ch>
2023-10-03 05:45:38 -0400

    Auto commit

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 //error_reporting(0);
 require_once 'vendor/autoload.php';
-use Laminas\Config\Config as Config;
-
-use Symfony\Component\Routing\Generator\UrlGenerator;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RouteCollection;
 /*
 use Whoops\Handler\JsonResponseHandler;
 use Whoops\Handler\PrettyPageHandler;
@@ -18,31 +11,6 @@ $whoops->register();
 */
 $ConfigFile = 'assets-php/settings.php';
 require 'assets-php/classes.php';
-
-
-
-$route = new Route('/blog/{slug}', ['_controller' => BlogController::class]);
-$routes = new RouteCollection();
-$routes->add('blog_show', $route);
-
-$context = new RequestContext();
-
-// Routing can match routes with incoming requests
-$matcher = new UrlMatcher($routes, $context);
-$parameters = $matcher->match('/blog/lorem-ipsum');
-// $parameters = [
-//     '_controller' => 'App\Controller\BlogController',
-//     'slug' => 'lorem-ipsum',
-//     '_route' => 'blog_show'
-// ]
-
-// Routing can also generate URLs for a given route
-$generator = new UrlGenerator($routes, $context);
-$url = $generator->generate('blog_show', [
-    'slug' => 'my-blog-post',
-]);
-print $url;
-exit;
 
 /* Request Handler */
 $Request = new Request($_REQUEST);
