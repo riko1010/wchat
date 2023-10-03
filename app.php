07ec2f52bea387bf3205673618fdebaa17a06045
riko tenten <riko1010@protonmail.ch>
2023-10-03 16:40:49 -0400

    Auto commit

--- app.php
+++ app.php
@@ -3,6 +3,8 @@ require_once 'vendor/autoload.php';
 
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
    
+   $r->addRoute('GET', '/{queryarg}/{paginationfrom:\d+}[-{paginationto:\d+}]', ['Controller', 'RouteIndex']);
+   $r->addRoute('GET', '/{queryarg}/{paginationfrom:\d+}-[]', ['Controller', 'RouteIndex']);
    
    $r->addRoute('GET', '/{queryarg}[/]', ['Controller', 'RouteIndex']);
    
