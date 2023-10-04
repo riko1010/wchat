824d474d903b3f32226df1cac790a0a7c13f5881
riko tenten <riko1010@protonmail.ch>
2023-10-04 05:20:08 -0400

    Auto commit

--- index.php
+++ index.php
@@ -51,108 +51,108 @@ Script
   include assets-js/classes.js
   
 :javascript
- $(document).ready(function(){
-     // setup Tooltips
-   $(".searchselect").select2(); 
-   //select chat, redirect to selection
-   $('.searchselect').on('select2:select', function (e) {
-    window.location.href = "./"+$(this).val();
+  $(document).ready(function(){
+  // setup Tooltips
+  $(".searchselect").select2(); 
+  //select chat, redirect to selection
+  $('.searchselect').on('select2:select', function (e) {
+  window.location.href = "./"+$(this).val();
   });
-});
-
-//<!-- Google Tag Manager -->
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MH4G76CM');</script>
-//<!-- End Google Tag Manager -->
+  });
+  
+  //<!-- Google Tag Manager -->
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MH4G76CM');</script>
+  //<!-- End Google Tag Manager -->
 
 body
-<!-- Google Tag Manager (noscript) -->
-  noscript
-    iframe(src="https://www.googletagmanager.com/ns.html?id=GTM-MH4G76CM" height="0" width="0" style="display:none;visibility:hidden")
-header(class="brand")
-  select(style="" class="form-select form-control searchselect")
-    <?= $App->Menu($Init) ?>           
-main(class='container h-100')
-  <?php if ($Init->Data->IsEmpty) { ?>
-  div(class="row")
-  div(class="col card")
-  cannot display chat files at this time - 
-  <a href="" class="reloadpage">retry</a>
-  <?php } ?>
-
-<?php if (!$Init->Data->IsEmpty) { ?>
-section
-  h3(class='chat-title'>WhatsApp Chat with <?= $App->Name ?>')
-  div class='row p-1 spotlight-group' id='whatsappimages'>
-<?= $processLines->ProcessAndPrint($Config, $App) ?>
-paginationnav(id="ArchivedNav")
-  <?= $App->PaginationNav($Config, $App) ?>
-  div(id="loader")
-  div(class="loader hidden d-flex justify-content-center")
-  <img src="icons/loader.gif" alt="Loading">
-
-nav(hidden)
-foreach ($Init->Data->IsEmpty as $link) {
-  print '<a href="' . $link['url'] . '">' . $link['url'] . '</a> <br />';
-}
-
-div(class="modal fade" id="sitemapModal" tabindex="-1" aria-labelledby="sitemapModalLabel" aria-hidden="true")
-  div(class="modal-dialog modal-dialog-centered modal-dialog-scrollable")
-    div(class="modal-content")
-      div(class="modal-header")
-        h1(class="modal-title fs-5" id="sitemapModalLabel")Sitemap
-        button(type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close")
-      div(class="modal-body")
-
-ul(class="list-group list-group-flush")
-<?php if (isset($sitemaps->files)) {
-  foreach ($sitemaps->files as $sitemap) {
-    if ($sitemap['exists'] == false) {
-      continue;
-    } ?>   
-  <a href="<?= $sitemap[
-    'rpath'
-  ] ?>" class="link-body-emphasis list-group-item list-group-item-action d-flex justify-content-between align-items-start" target="_blank">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">
-      <?= $sitemap['filename'] ?>
-        </div>
-<?= isset($sitemap['stats'])
-  ? getFilesize($sitemap['stats']['size'], 'human')
-  : '' ?>
-    </div>
-    span(class="badge bg-success rounded-pill")
-    <?= isset($sitemap['stats'])
-      ? date('M d, Y', $sitemap['stats']['mtime'])
-      : '' ?>
-<?php
-  }
-} ?>
-    span(class="link-body-emphasis list-group-item list-group-item-action d-flex justify-content-between align-items-start")
-
-    div(class="ms-2 me-auto")
-
-      div(class="fw-bold")
-      <?= $sitemaps->sitemap->status ? 'OK' : 'ERROR' ?>
-        
-<?= $sitemaps->sitemap->response ?>
-    
-      span(class="badge bg-success rounded-pill")
-      <?= date('M d, Y h:i:sa', time()) ?>
-
-      div(class="modal-footer")
-        button(type="button" class="btn btn-secondary" data-bs-dismiss="modal")Close
-
-    footer
-      <?= $App->eof ?:
-        $Config->NPaginationFrom .
-          ',' .
-          $Config->NPaginationTo ?> <a href="sitemap.xml" data-bs-toggle="modal" data-bs-target="#sitemapModal">sitemap</a> | from <a target="_blank" href="https://github.com/itxshakil/Whatsapp-backup-Viewer">Shakil Alam on Github</a>
+  <!-- Google Tag Manager (noscript) -->
+    noscript
+      iframe(src="https://www.googletagmanager.com/ns.html?id=GTM-MH4G76CM" height="0" width="0" style="display:none;visibility:hidden")
+  header(class="brand")
+    select(style="" class="form-select form-control searchselect")
+      <?= $App->Menu($Init) ?>           
+  main(class='container h-100')
+    <?php if ($Init->Data->IsEmpty) { ?>
+    div(class="row")
+    div(class="col card")
+    cannot display chat files at this time - 
+    <a href="" class="reloadpage">retry</a>
+    <?php } ?>
   
-<script>
+  <?php if (!$Init->Data->IsEmpty) { ?>
+  section
+    h3(class='chat-title'>WhatsApp Chat with <?= $App->Name ?>')
+    div class='row p-1 spotlight-group' id='whatsappimages'>
+  <?= $processLines->ProcessAndPrint($Config, $App) ?>
+  paginationnav(id="ArchivedNav")
+    <?= $App->PaginationNav($Config, $App) ?>
+    div(id="loader")
+    div(class="loader hidden d-flex justify-content-center")
+    <img src="icons/loader.gif" alt="Loading">
+  
+  nav(hidden)
+  foreach ($Init->Data->IsEmpty as $link) {
+    print '<a href="' . $link['url'] . '">' . $link['url'] . '</a> <br />';
+  }
+  
+  div(class="modal fade" id="sitemapModal" tabindex="-1" aria-labelledby="sitemapModalLabel" aria-hidden="true")
+    div(class="modal-dialog modal-dialog-centered modal-dialog-scrollable")
+      div(class="modal-content")
+        div(class="modal-header")
+          h1(class="modal-title fs-5" id="sitemapModalLabel")Sitemap
+          button(type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close")
+        div(class="modal-body")
+  
+  ul(class="list-group list-group-flush")
+  <?php if (isset($sitemaps->files)) {
+    foreach ($sitemaps->files as $sitemap) {
+      if ($sitemap['exists'] == false) {
+        continue;
+      } ?>   
+    <a href="<?= $sitemap[
+      'rpath'
+    ] ?>" class="link-body-emphasis list-group-item list-group-item-action d-flex justify-content-between align-items-start" target="_blank">
+      <div class="ms-2 me-auto">
+        <div class="fw-bold">
+        <?= $sitemap['filename'] ?>
+          </div>
+  <?= isset($sitemap['stats'])
+    ? getFilesize($sitemap['stats']['size'], 'human')
+    : '' ?>
+      </div>
+      span(class="badge bg-success rounded-pill")
+      <?= isset($sitemap['stats'])
+        ? date('M d, Y', $sitemap['stats']['mtime'])
+        : '' ?>
+  <?php
+    }
+  } ?>
+      span(class="link-body-emphasis list-group-item list-group-item-action d-flex justify-content-between align-items-start")
+  
+      div(class="ms-2 me-auto")
+  
+        div(class="fw-bold")
+        <?= $sitemaps->sitemap->status ? 'OK' : 'ERROR' ?>
+          
+  <?= $sitemaps->sitemap->response ?>
+      
+        span(class="badge bg-success rounded-pill")
+        <?= date('M d, Y h:i:sa', time()) ?>
+  
+        div(class="modal-footer")
+          button(type="button" class="btn btn-secondary" data-bs-dismiss="modal")Close
+  
+      footer
+        <?= $App->eof ?:
+          $Config->NPaginationFrom .
+            ',' .
+            $Config->NPaginationTo ?> <a href="sitemap.xml" data-bs-toggle="modal" data-bs-target="#sitemapModal">sitemap</a> | from <a target="_blank" href="https://github.com/itxshakil/Whatsapp-backup-Viewer">Shakil Alam on Github</a>
+    
+  script
 dev = true;
  /* archive.org modifies js, jwuery, functions are available, classes do not seem available, suggestgroupchat will ot utlize classes until solution found */<?php if (
    $Config->SuggestGroupChat == true
