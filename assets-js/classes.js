18c027659071cac8b94c196d68dc91bfd6d226e5
riko tenten <riko1010@protonmail.ch>
2024-02-20 08:40:51 -0500

    Auto commit dev

--- assets-js/classes.js
+++ assets-js/classes.js
@@ -142,17 +142,11 @@ $.ajaxQ = (function(){
 
 })();
 
-const stringToColour = (str: string) => {
-  let hash = 0;
-  str.split('').forEach(char => {
-    hash = char.charCodeAt(0) + ((hash << 5) - hash)
-  })
-  let colour = '#'
-  for (let i = 0; i < 3; i++) {
-    const value = (hash >> (i * 8)) & 0xff
-    colour += value.toString(16).padStart(2, '0')
-  }
-  return colour
+function getBackgroundColor(stringInput) {
+    let stringUniqueHash = [...stringInput].reduce((acc, char) => {
+        return char.charCodeAt(0) + ((acc << 5) - acc);
+    }, 0);
+    return `hsl(${stringUniqueHash % 360}, 95%, 35%)`;
 }
 
 function devlog(str){
