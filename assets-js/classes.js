95dea0c298a7c7a19ad08edbd1a766cb738fef3e
riko tenten <riko1010@protonmail.ch>
2024-02-20 08:35:28 -0500

    Auto commit dev

--- assets-js/classes.js
+++ assets-js/classes.js
@@ -142,6 +142,19 @@ $.ajaxQ = (function(){
 
 })();
 
+const stringToColour = (str: string) => {
+  let hash = 0;
+  str.split('').forEach(char => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash)
+  })
+  let colour = '#'
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff
+    colour += value.toString(16).padStart(2, '0')
+  }
+  return colour
+}
+
 function devlog(str){
   if (dev == true)  console.log(str);
 }
