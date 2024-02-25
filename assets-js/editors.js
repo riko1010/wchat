6f778e8da2fcc85313b13911ffd8c92f715a28fc
riko tenten <riko1010@protonmail.ch>
2024-02-25 09:55:29 -0500

    Auto commit

--- assets-js/editors.js
+++ assets-js/editors.js
@@ -1,10 +1,6 @@
 /* headjs */
 /* headjs loads, on ready -> */
 head(function () {
-  
 /*  wysiwyg editor */
-tinymce.init({
-  $('#annotation-update-input').trumbowyg();
-/* end wysiwyg editor */   
-})
-
+$('#annotation-update-input').trumbowyg();
+});
