955ac8e3497b2e97088931b4b7ff4fa5a4f94753
riko tenten <riko1010@protonmail.ch>
2024-02-25 01:05:49 -0500

    Auto commit

--- assets-js/index.js
+++ assets-js/index.js
@@ -99,11 +99,12 @@ head(function () {
 
 $(document).ready(function () {
 
-  /* change header position to fixed on load */
-  $('#header').css({
-      'position' : 'fixed'
-    });  
-    
+/* change header position to fixed on load */
+$('#header').css({
+  'position' : 'fixed'
+});  
+/* end change header position to fixed on load */
+
 /* chat-container top margin, header is sticky on load, changes to fixed and add top margin of calculated header height to chat-container */ 
 $('.chat-container').css({
 'margin-top' : $('#header').outerHeight() + 'px'
