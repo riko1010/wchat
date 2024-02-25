38ef8258461e0234f4141c86ddcfa32304146839
riko tenten <riko1010@protonmail.ch>
2024-02-25 06:10:46 -0500

    Auto commit

--- assets-js/admin.js
+++ assets-js/admin.js
@@ -63,17 +63,12 @@ window.onmessage = function(e) {
 /* headjs loads, on ready -> */
 head(function () {
 
-/* tinymce wysiwyg editor */
-  tinymce.init({
-        selector: '#annotation-update-input',
-        menubar: 'edit view insert format',
-        plugins: [
-  'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
-  'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
-  'media', 'table', 'emoticons', 'template', 'help'
-],
-      });
-/* end tinymce wysiwyg editor */      
+/*  wysiwyg editor */
+const quill = new Quill('#annotation-update-input', {
+    theme: 'snow'
+  });
+/* end wysiwyg editor */      
+
 /* copy admin chatfile link */
    var clipboard3 = new ClipboardJS(".AdminChatfileLink", {
       container: document.getElementById('adminModal'),
