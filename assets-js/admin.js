ef9cee51e00c5cfb9e1eb153b20405eb9778c289
riko tenten <riko1010@protonmail.ch>
2024-02-27 13:30:18 -0500

    Auto commit dev

--- assets-js/admin.js
+++ assets-js/admin.js
@@ -35,21 +35,7 @@ $(".logout").on('click', function (e) {
       $("#AdminPassword").val(),
     );
   });
-  
- /* uploadchatfilearchive */
-  
-  
-/* selected file validation */  
-$('body').on('change', '#chatfilearchive', function(){
-var fileExtension = ['zip'];
-if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
-  $('.uploadchatfilearchive').prop('disabled', true);    
-  $('.chatfilearchivenotice').html('<span style="color:red;">Only these formats are allowed: '+fileExtension.join(', ')+'</span>');
-        } else {
-  $('.uploadchatfilearchive').prop('disabled', false);       
-  $('.chatfilearchivenotice').text($(this).val());        
-        }
-});  
+
 /* listen for loggedin status */
 window.onmessage = function(e) {
     if (e.data == 'notloggedin') {
