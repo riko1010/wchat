90ff471d9bfd69b3290b5ecff09be8d559b5c943
riko tenten <riko1010@protonmail.ch>
2024-02-28 07:15:44 -0500

    Auto commit dev

---
+++ assets-php/WChat/Controller.php
@@ -0,0 +1,347 @@
+<?php
+namespace WChat;
+class Controller {
+
+public function RouteIndex(
+  \Psr\Container\ContainerInterface $c,  
+  ) {
+/* instantiation, ordered */
+$Config = $c->get('WChat\Config');
+$Init = $c->get('WChat\Init');
+$sitemap = $c->get('WChat\generateSiteMap');
+$App = $c->get('WChat\App');
+
+$loader = new \Twig\Loader\FilesystemLoader(pj($Config->baseDir, 'assets-templates'));
+/* idk why caching was disabled */
+//'cache' => pj($Config->baseDir, '/assets-templates/cache'),
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+$function = new \Twig\TwigFunction('getFilesize', function (...$args) {
+    return \getFilesize(...$args);
+});
+$twig->addFunction($function);
+$function = new \Twig\TwigFunction('screenshot', function ($screenshotapi, $pageuri) {
+    return sprintf($screenshotapi, urlencode($pageuri),);
+});
+$twig->addFunction($function);
+
+print $twig->render('index.twig', [
+    'App' => $App,
+    'Config' => $Config,
+    'Init' => $Init,
+    'sitemap' => $sitemap,
+    'session' => $_SESSION,
+    'container' => $c,
+  ]);
+
+}
+
+public function RouteAPI(
+  \Psr\Container\ContainerInterface $c,  
+  ) {
+/* instantiation, ordered */
+$Config = $c->get('WChat\Config');
+$Request = $c->get('WChat\Request');
+$RequestGet = new \WChat\Request($_GET, true);
+$Config->InitType = 'API';
+/* terminate if no queryarg */
+if (empty($RequestGet->queryarg)) exit('Chat ID is empty.');
+
+$Request->queryarg = $RequestGet->queryarg;
+$Request->paginationfrom = $RequestGet->paginationfrom;
+$Request->needle = $RequestGet->needle;
+
+$Init = $c->get('WChat\Init');
+$App = $c->get('WChat\App');
+/* groupchat displays both sender names by default, prior to search styling similar to whatsapp styling */
+$App->GroupChat = true;
+
+if ($App->NoSelected) exit('Chat ID not found. ');
+
+$c->call(['WChat\processLines', 'ProcessSearchAndPrint']);
+
+}
+
+
+public function RouteANNOTATION(
+  \Psr\Container\ContainerInterface $c,  
+  ) {
+/* instantiation, ordered */
+$Config = $c->get('WChat\Config');
+$Request = $c->get('WChat\Request');
+$db = $c->get('WChat\Database');
+$RequestPost = new \WChat\Request($_POST, true);
+$Config->InitType = 'ANNOTATION';
+
+/* if request pattern is incorrect */
+if (
+  !isset($RequestPost->updateannotation) ||
+  $RequestPost->updateannotation !== 'yes' || 
+  empty($RequestPost->annotation) || 
+  $_SESSION['users_id'] == null
+  ) {
+  print json_encode(['status' => 'error', 'response' => 'error - empty fields']);
+  exit;
+}
+
+try {
+$RawAnnotation = $_POST['annotation'];
+/* sanitize using HTMLPurifier */
+$config = \HTMLPurifier_Config::createDefault();
+/* remove css properties to prevent malicious annotations */
+$config->set('CSS.AllowedProperties', '');
+$purifier = new \HTMLPurifier($config);
+$RawAnnotation = $purifier->purify($RawAnnotation);
+
+$InsertOrUpdate = $db->InsertOrUpdate(
+    'chatfiles',
+    [
+    'annotation' => $RawAnnotation,
+    ],
+    [ 
+    'id' => $RequestPost->annotationid,
+    'users_id' => $_SESSION['users_id'],
+    ],
+    'update'
+  );
+  
+  if (!$InsertOrUpdate->status) {
+    Throw new \Exception ($InsertOrUpdate->response);
+  } else {
+    print json_encode([
+        'status' => 'success',
+        'response' => $RawAnnotation,
+        ]); 
+        exit;
+  }
+  
+} catch (\Exception|\Throwable $e) {
+  print json_encode([
+        'status' => 'error',
+        'response' => 'Update failed:'.$e->getMessage()
+        ]); 
+  exit;      
+  }
+
+}
+
+public function RouteADMIN(
+  \Psr\Container\ContainerInterface $c,  
+  ) {
+/* instantiation, ordered */
+$Config = $c->get('WChat\Config');
+$Request = $c->get('WChat\Request');
+$db = $c->get('WChat\Database');
+$RequestPost = new \WChat\Request($_POST, true);
+$Config->InitType = 'ADMIN';
+
+/* logout */
+if (isset($RequestPost->logout) && $RequestPost->logout == 'yes') {
+  $_SESSION = [];
+  print json_encode(['status' => 'success', 'response' => 'Logged out.']);
+  exit;
+}
+
+/* upload chatfile */
+if (isset($RequestPost->uploadchatfile) && !empty($RequestPost->uploadchatfile)) {
+  print json_encode(['status' => 'success', 'response' => 'file upload.']);
+  exit;
+}
+
+/* login */
+if (empty($RequestPost->email) || empty($RequestPost->password)) { print json_encode(['status' => 'error', 'response' => 'Email or Password is empty.']);
+  exit;
+}
+
+$Request->email = $RequestPost->email;
+/* all user inputs are sanitized. special chars in password reasonably gets escaped/stripped using raw post data */
+$Request->password = $_POST['password'];
+
+/* if logged in */
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== false) {
+ /* logged in */
+  $AdminLoginQuery = $db->SelectOne(
+  'users',
+  ['email' => $_SESSION['email'],]
+  );
+} else {
+  /* log in */
+$AdminLoginQuery = $db->SelectOne(
+  'users',
+  [
+  'email' => $Request->email,
+  'password' => $Request->password
+  ]
+  );
+}
+/* login, fetch data, iterable to array */
+$AdminLoginQueryData = $AdminLoginQuery->status ? 
+  \BenTools\IterableFunctions\iterable_to_array($AdminLoginQuery->response)
+  : 
+  [];
+
+if (!empty($AdminLoginQueryData)) {
+/* assign session data, logged in */
+$_SESSION['loggedin'] = true;
+$_SESSION['users_id'] = $AdminLoginQueryData[0]['id'];
+$_SESSION['email'] = $AdminLoginQueryData[0]['email'];
+
+/* get users chatfiles */
+$ChatFilesQuery = $db->Select(
+  'chatfiles',
+  [
+  'users_id' => $_SESSION['users_id'],
+  ],
+  ['dateadded DESC']
+  );
+/* chatfiles iterable to array */
+$ChatFilesQueryData = $ChatFilesQuery->status ? 
+  \BenTools\IterableFunctions\iterable_to_array($ChatFilesQuery->response) 
+  : 
+  [];
+/* login successful, fetch successful, return email, chatfiles */
+print json_encode(['status' => 'success', 'response' => $AdminLoginQueryData[0]['email'], 'chatfiles' => $ChatFilesQueryData]);
+exit;
+}
+
+
+  /* login failed */
+print json_encode(['status' => 'error', 'response' => 'Email or Password is incorrect.']);
+
+/*
+$Init = $c->get('WChat\Init');
+$App = $c->get('WChat\App');
+
+$c->call(['WChat\processLines', 'ProcessSearchAndPrint']);
+*/
+
+}
+
+public function RouteIFRAMES(
+  \Psr\Container\ContainerInterface $c,  
+  ) {
+/* instantiation, ordered, some classes are unecessary for file upload */
+$Config = $c->get('WChat\Config');
+$Request = $c->get('WChat\Request');
+$db = $c->get('WChat\Database');
+
+$Init = $c->get('WChat\Init');
+$sitemap = $c->get('WChat\generateSiteMap');
+$App = $c->get('WChat\App');
+
+$RequestR = new \WChat\Request($_REQUEST, true);
+$Config->InitType = 'IFRAMES';
+
+/* upload form */
+if (isset($RequestR->uploadform) && $RequestR->uploadform == 'show') {
+$loader = new \Twig\Loader\FilesystemLoader(pj($Config->baseDir, 'assets-templates'));
+/* idk why caching was disabled */
+//'cache' => pj($Config->baseDir, '/assets-templates/cache'),
+$twig = new \Twig\Environment($loader, [
+  'cache' => false,
+]);
+
+print $twig->render('uploadchatfiles.twig', [
+    'App' => $App,
+    'Config' => $Config,
+    'session' => $_SESSION,
+    'container' => $c,
+  ]);
+  exit;
+}
+
+/* upload chatfile */
+if (empty($_SESSION['users_id'])) {
+  print json_encode(['status' => 'error', 'response' => 'You\'re not logged in']);
+  exit;
+}
+
+/* upload chatfile */
+if (!empty($RequestR->submitupload=true)) {
+  print json_encode(['status' => 'success', 'response' => 'file upload..']);
+  
+  var_dump($_FILES);
+  print '<hr/>';
+  $_FILES["chatfilearchive"]["name"];
+  $_FILES["chatfilearchive"]["tmp_name"];
+  
+  $zip_file = $_FILES["chatfilearchive"]["tmp_name"];
+  if (file_exists($zip_file)) print 'exists';
+//  '/path/to/file.zip'; 
+// I wan to get stream a CSV files
+
+$zip = new \ZipArchive();
+$zip->open($zip_file);
+for ($i = 0; $i < $zip->numFiles; $i++) {
+// Check file by file
+    $name = $zip->getNameIndex($i);
+    print $name.'<br/>';
+    // Retrieve entry name
+    $extension = pathinfo($name, PATHINFO_EXTENSION);
+    if ($extension !== 'png') {
+    // I want to handle csv files
+        $stream = $zip->getStream($name); // No stream index access before PHP 8.2
+        $zip->extractTo($Config->baseDir.'/test', $name);
+        print 'extracted'.$name.$stream.'<br/>';
+        // Starting PHP 8.2 $zip->getStreamIndex() or $zip->getStreamName()
+        // Do stuff with $stream
+        // ...
+    }
+}
+print '<br/';
+//$zip->close();
+  //end
+  
+  $zip2 = new \ZipArchive();
+  $zip2->open($zip_file);
+  for ($i = 0; $i < $zip2->numFiles; $i++) {
+// Check file by file
+    $name = $zip2->getNameIndex($i);
+    print $name.'<br/>';
+  }
+ // $zip->close();
+  
+  
+  exit;
+}
+
+}
+
+
+public function RouteSEARCH(
+  \Psr\Container\ContainerInterface $c,  
+  ) {
+/* instantiation, ordered, some classes are unecessary for file upload */
+$Config = $c->get('WChat\Config');
+$Request = $c->get('WChat\Request');
+$db = $c->get('WChat\Database');
+
+$Init = $c->get('WChat\Init');
+$sitemap = $c->get('WChat\generateSiteMap');
+$App = $c->get('WChat\App');
+
+$RequestR = new \WChat\Request($_REQUEST, true);
+$Config->InitType = 'SEARCH';
+
+$loader = new \Twig\Loader\FilesystemLoader(pj($Config->baseDir, 'assets-templates'));
+/* idk why caching was disabled */
+//'cache' => pj($Config->baseDir, '/assets-templates/cache'),
+$twig = new \Twig\Environment($loader, [
+  'cache' => false,
+]);
+
+print $twig->render('search.twig', [
+    'App' => $App,
+    'Config' => $Config,
+    'session' => $_SESSION,
+    'container' => $c,
+    'Init' => $Init,
+    'RequestR' => $RequestR,
+  ]);
+  
+
+}
+
+}
+?>
