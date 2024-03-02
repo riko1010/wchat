ea0975256d602b97880c2bddf35ed393cc6b98a6
riko tenten <riko1010@protonmail.ch>
2024-03-02 07:20:20 -0500

    Auto commit

--- assets-js/search.js
+++ assets-js/search.js
@@ -155,7 +155,7 @@ $('.searchchatsresults').removeClass('hidden');
  
   /* show navicons, hide searchbox and searchresults */
     $('.chat-container, .wchatlogo, select .searchselect').on('click', function(){
-      $('.searchchatsresults .searchchatsresults').addClass('hidden');
+      $('.searchchatsresults .searchchatsshowmore').addClass('hidden');
       $('.searchchats-container, header-search').slideUp(); 
   
     $('.navicons').slideDown();  
