5342e5f2f1a522af9b19eca813441a730c6603a6
riko tenten <riko1010@protonmail.ch>
2024-03-06 12:20:11 -0500

    Auto commit

--- assets-js/header.js
+++ assets-js/header.js
@@ -21,15 +21,15 @@ var headroom  = new Headroom(myElement, options);
 headroom.init();
 /* end headroom */
 
-/* change header position to fixed on load 
+/* change header position to fixed on load */
 $('#header').css({
   'position' : 'fixed'
 });  
-end change header position to fixed on load */
+/* end change header position to fixed on load */
 
-/* chat-container top margin, header is sticky on load, changes to fixed and add top margin of calculated header height to chat-container 
+/* chat-container top margin, header is sticky on load, changes to fixed and add top margin of calculated header height to chat-container  */
 $('.chat-container').css({
 'margin-top' : $('#header').outerHeight() + 'px'
 });
-end chat-container header */
+/* end chat-container header */
 });
