a80158511e58ec65a80545d3fb107271625027ae
riko tenten <riko1010@protonmail.ch>
2024-03-31 03:30:34 -0400

    Auto commit

--- assets-js/uploadchatfiles.js
+++ assets-js/uploadchatfiles.js
@@ -14,14 +14,14 @@ if (SessionLoggedIn === false) {
 /* selected file validation */  
 $('body').on('change', '#chatfilearchive', function(e){
   /* allowed extensions */
-var fileExtension = ['zip', 'gz', '7z', 'rar'];
+var fileExtension = ['zip'];
 if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
   $('.uploadchatfilearchive').prop('disabled', true);    
   $('.chatfilearchivenotice').html('<span style="color:red;">Only these formats are allowed: '+fileExtension.join(', ')+'</span>');
         } else {
   try {
   var selectedfilename = e.target.files[0].name;
-  selectedfilename = selectedfilename.split('.');
+  selectedfilename = selectedfilename.split('.zip');
   $('.uploadchatfilearchive').prop('disabled', false);       
   $('#FolderName').val(selectedfilename[0]);  
   } catch(e) {
